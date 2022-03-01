2a023dc976072b417dbdb23e4f5859044bf6657c
wolfo1 <70093754+wolfo1@users.noreply.github.com>
2022-03-01 02:14:26 +0200

    Update WordleSolver.py
    bug fixed. hooray!

--- WordleSolver.py
+++ WordleSolver.py
@@ -2,12 +2,16 @@ import itertools
 import sys
 import re
 
+
 HEBREW_LETTERS = "אבגדהוזחטיכלמנסעפצקרשת"
 ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz"
 FINAL_TO_REGULAR = {'ך': 'כ', 'ם': 'מ', 'ן': 'נ', 'ף': 'פ', 'ץ': 'צ'}
 REGULAR_TO_FINAL = {'כ': 'ך', 'מ': 'ם', 'נ': 'ן', 'פ': 'ף', 'צ': 'ץ'}
 KEYWORDS = [''.join(i) for i in itertools.product("012", repeat=5)]
 REGEX = "[{a}]{{1}}[{b}]{{1}}[{c}]{{1}}[{d}]{{1}}[{e}]{{1}}"
+GREEN = '2'
+YELLOW = '1'
+GREY = '0'
 
 
 def filter_words(text: str, filters: list) -> bool:
@@ -24,30 +28,35 @@ def getWordList(filename) -> list[str]:
 
 def calculateExpectedValue(word, wordlist) -> float:
     results = []
-    for i in range(1):
+    for i in range(243):
         pattern = KEYWORDS[i]
-        pattern = "01210"
         print("pattern: " + pattern)
-        #letters = [HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS]
+        # letters = [HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS]
         letters = [ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS]
+        # mustHave holds how many occurrences each letter has to appear in the solution (yellow or green letters)
         # mustHave = {"א": 0, "ב": 0, "ג": 0, "ד": 0, "ה": 0, "ו": 0, "ז": 0, "ח": 0, "ט": 0, "י": 0, "כ": 0,
         #            "ל": 0, "מ": 0, "נ": 0, "ס": 0, "ע": 0, "פ": 0, "צ": 0, "ק": 0, "ר": 0, "ש": 0, "ת": 0}
         mustHave = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0,
                     'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0,
                     'y': 0, 'z': 0}
+        greys = set()
         for j in range(5):
-            if pattern[j] == '0':
-                for k in range(5):
-                    letters[k] = letters[k].replace(word[j], "", 1)
-            elif pattern[j] == '1':
+            if pattern[j] == GREY:
+                # only add letter to greys if it hasn't appeared as yellow \ green before.
+                if mustHave[word[j]] == 0:
+                    greys.add(word[j])
+            elif pattern[j] == YELLOW:
                 letters[j] = letters[j].replace(word[j], "")
-
+                greys.discard(word[j])
                 mustHave[word[j]] += 1
-            elif pattern[j] == '2':
+            elif pattern[j] == GREEN:
                 letters[j] = word[j]
-        fltrs = [(k, v) for k, v in mustHave.items() if v > 0]
-        print(fltrs)
+                mustHave[word[j]] += 1
+        print(mustHave)
         print(letters)
+        for j in range(5):
+            letters[j] = ''.join('' if c in greys else c for c in letters[j])
+        fltrs = [(k, v) for k, v in mustHave.items() if v > 0]
         r = REGEX.format(a=letters[0], b=letters[1], c=letters[2], d=letters[3], e=letters[4])
         regex = re.compile(r)
         newWordList = list(filter(regex.match, wordlist))
