1e6f56401508ff971f8d22528baebc9ab87059ca
wolfo1 <70093754+wolfo1@users.noreply.github.com>
2022-03-01 02:57:05 +0200

    Update WordleSolver.py
    Double / tripled lettered words should work as intended now.

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,7 +1,7 @@
 import itertools
 import sys
 import re
-
+from collections import defaultdict
 
 HEBREW_LETTERS = "אבגדהוזחטיכלמנסעפצקרשת"
 ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz"
@@ -14,55 +14,89 @@ YELLOW = '1'
 GREY = '0'
 
 
-def filter_words(text: str, filters: list) -> bool:
-    return all(text.count(character) == frequency
-               for character, frequency in filters)
-
-
 def getWordList(filename) -> list[str]:
+    """
+    get word list from a given filepath.
+    :param filename:
+    :return: list of words.
+    """
     f = open(filename, "r", encoding="utf-8")
     wordlist = f.read().splitlines()
     f.close()
     return wordlist
 
 
+def atLeast_filter(word: str, filters: list[tuple[str, int]]) -> bool:
+    """
+    function checks if the word has each letter at least the number of occurrences specified in the filters.
+    :param word: a string.
+    :param filters: a list of tuples, each containing a character and number of occurrences.
+    :return: true if word follows the filters, false otherwise.
+    """
+    return all(word.count(character) >= frequency
+               for character, frequency in filters)
+
+
+def exactly_filter(word: str, filters: list[tuple[str, int]]) -> bool:
+    """
+    function checks if the word has each letter at least the number of occurrences specified in the filters.
+    :param word: a string.
+    :param filters: a list of tuples, each containing a character and number of occurrences.
+    :return: true if word follows the filters, false otherwise.
+    """
+    return all(word.count(character) == frequency
+               for character, frequency in filters)
+
+
 def calculateExpectedValue(word, wordlist) -> float:
+    """
+    function calculates what is the expected information that will be given by the word for a given list of words.
+    :param word: word to calculate expected information from.
+    :param wordlist: list of possible solutions.
+    :return: float, what is the expected information. lower is better.
+    """
     results = []
+    # iterate over all possible patterns (grey, yellow, green)
     for i in range(243):
         pattern = KEYWORDS[i]
-        print("pattern: " + pattern)
         # letters = [HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS]
         letters = [ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS]
-        # mustHave holds how many occurrences each letter has to appear in the solution (yellow or green letters)
+        # holds how many occurrences each letter has to appear in the solution. 0 is unknown.
         # mustHave = {"א": 0, "ב": 0, "ג": 0, "ד": 0, "ה": 0, "ו": 0, "ז": 0, "ח": 0, "ט": 0, "י": 0, "כ": 0,
         #            "ל": 0, "מ": 0, "נ": 0, "ס": 0, "ע": 0, "פ": 0, "צ": 0, "ק": 0, "ר": 0, "ש": 0, "ת": 0}
-        mustHave = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0,
-                    'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0,
-                    'y': 0, 'z': 0}
-        greys = set()
+        # letters that have to appear exactly number of times.
+        exactly = defaultdict(int)
+        # letters that have to appear at least number of times
+        atleast = defaultdict(int)
+        greys = []
         for j in range(5):
             if pattern[j] == GREY:
-                # only add letter to greys if it hasn't appeared as yellow \ green before.
-                if mustHave[word[j]] == 0:
-                    greys.add(word[j])
+                letters[j] = letters[j].replace(word[j], "")
+                greys.append(word[j])
             elif pattern[j] == YELLOW:
                 letters[j] = letters[j].replace(word[j], "")
-                greys.discard(word[j])
-                mustHave[word[j]] += 1
+                atleast[word[j]] += 1
             elif pattern[j] == GREEN:
                 letters[j] = word[j]
-                mustHave[word[j]] += 1
-        print(mustHave)
-        print(letters)
+                atleast[word[j]] += 1
+        # if a letter was greyed & yellowed / green, move it to "exactly".
+        for letter in greys:
+            if atleast[letter] > 0:
+                exactly[letter] = atleast[letter]
+                greys.remove(letter)
+        # remove all completely greyed chars from possible letters.
         for j in range(5):
             letters[j] = ''.join('' if c in greys else c for c in letters[j])
-        fltrs = [(k, v) for k, v in mustHave.items() if v > 0]
+        atLeast_filters = [(k, v) for k, v in atleast.items() if v > 0]
+        exactly_filters = [(k, v) for k, v in exactly.items() if v > 0]
+        # filter out words that don't complie with possible chars for each letter.
         r = REGEX.format(a=letters[0], b=letters[1], c=letters[2], d=letters[3], e=letters[4])
         regex = re.compile(r)
         newWordList = list(filter(regex.match, wordlist))
-        newWordList = list(filter(lambda word: filter_words(word, fltrs), newWordList))
+        # filter out based on atLeast and exactly dicts.
+        newWordList = list(filter(lambda word: atLeast_filter(word, atLeast_filters), newWordList))
+        newWordList = list(filter(lambda word: exactly_filter(word, exactly_filters), newWordList))
         results.append(len(newWordList))
-        print(newWordList)
     average = sum(results) / len(results)
     return average
 
