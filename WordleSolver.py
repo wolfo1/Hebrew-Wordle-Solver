624cd96ee071232743e9a487f4f453e23bcdd302
wolfo1 <70093754+wolfo1@users.noreply.github.com>
2022-05-28 18:50:58 +0300

    Update
    Fixed bugs (wrong entropy calculation for Hebrew words with a finalized letter).
    Moved from txt files to JSON files.
    Calculate_first_words now write result to JSON file instead of printing them.

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,60 +1,72 @@
 import itertools
 import math
 import re
+import json
+from typing import List
 from collections import defaultdict
+from enum import Enum
 
 WORD_LENGTH = 5
 NUM_OF_TRIES = 6
 HEBREW_LETTERS = "אבגדהוזחטיכלמנסעפצקרשת"
-HEBREW_GUESSES = "HebrewSolutionList.txt"
-HEBREW_SOLUTIONS = "HebrewSolutionList.txt"
 ENGLISH_LETTERS = "abcdefghijklmnopqrstuvwxyz"
-ENGLISH_GUESSES = "EnglishGuessList.txt"
-ENGLISH_SOLUTIONS = "EnglishSolutionList.txt"
-FIRST_ENGLISH_GUESS = [('soare', 5.885), ('roate', 5.884), ("raise", 5.877), ('reast', 5.867), ('raile', 5.865), ('slate', 5.855), ('salet', 5.836)]
-FIRST_HEBREW_GUESS = [('מילות', 5.985), ('מניות', 5.983), ('הורית', 5.960), ('מונית', 5.938), ('משרות', 5.935)]
+
+# data files
+HEBREW_GUESSES_PATH = "database/hebrew_solutions.json"
+HEBREW_SOLUTIONS_PATH = "database/hebrew_solutions.json"
+ENGLISH_GUESSES_PATH = "database/english_guesses.json"
+ENGLISH_SOLUTIONS_PATH = "database/english_solutions.json"
+FIRST_WORDS_ENGLISH_PATH = "database/english_first_word_scores.json"
+FIRST_WORDS_HEBREW_PATH = "database/hebrew_first_word_scores.json"
+
+# dict from finalised letters to regular and vice versa
 FINAL_TO_REGULAR = {'ך': 'כ', 'ם': 'מ', 'ן': 'נ', 'ף': 'פ', 'ץ': 'צ'}
 REGULAR_TO_FINAL = {'כ': 'ך', 'מ': 'ם', 'נ': 'ן', 'פ': 'ף', 'צ': 'ץ'}
 REGEX = "[{a}]{{1}}[{b}]{{1}}[{c}]{{1}}[{d}]{{1}}[{e}]{{1}}"
 GREEN = '2'
 YELLOW = '1'
 GREY = '0'
+FAIL_MSG = "Failed to find the word in number of tries: "
 
 
-def get_word_list(filename: str) -> list[str]:
+class Language(Enum):
+    HEBREW = 0
+    ENGLISH = 1
+
+
+def load_words_JSON(filepath: str) -> list[str]:
     """
     get word list from a given filepath.
-    :param filename:
+    :param filepath: path to a JSON file
     :return: list of words.
     """
-    f = open(filename, "r", encoding="utf-8")
-    wordlist = f.read().splitlines()
-    f.close()
-    return wordlist
+    with open(filepath, encoding='utf-8') as f:
+        data = json.load(f)
+    return data['words']
 
 
-def process_hebrew_word(word: str) -> str:
+def atLeast_filter(word: str, char_frequencies: list[tuple[str, int]]) -> bool:
     """
-    turn the last letter to finalised ("ot sofit") later or vice versa.
-    :param word: a hebrew word
-    :return: the word with a finalised / de-finalised last letter.
+    check if all characters in a given word appear at least the number of times desired.
+    :param word: the word to check
+    :param char_frequencies: a list of all characters, and their desired minimal frequency
+    :return: True if all characters in word appear at least the number of times specified
+            in char_frequencies, else False
     """
-    if word[-1] in REGULAR_TO_FINAL:
-        return word[:-1] + REGULAR_TO_FINAL[word[-1]]
-    elif word[-1] in FINAL_TO_REGULAR:
-        return word[:-1] + FINAL_TO_REGULAR[word[-1]]
-    else:
-        return word
-
-
-def atLeast_filter(word: str, filters: list[tuple[str, int]]) -> bool:
     return all(word.count(character) >= frequency
-               for character, frequency in filters)
+               for character, frequency in char_frequencies)
 
 
-def exactly_filter(word: str, filters: list[tuple[str, int]]) -> bool:
+def exactly_filter(word: str, char_frequencies: list[tuple[str, int]]) -> bool:
+    """
+    check if all characters in a given word appear exactly the number of times desired.
+    :param word: the word to check
+    :param char_frequencies: a list of all characters, and their desired frequency
+    :return: return True if all characters in word appear exactly the number of times
+            specified in char_frequencies list, else False.
+    """
     return all(word.count(character) == frequency
-               for character, frequency in filters)
+               for character, frequency in char_frequencies)
 
 
 def get_pattern(guess: str, solution: str) -> str:
@@ -87,9 +99,19 @@ def get_pattern(guess: str, solution: str) -> str:
     return ''.join(pattern)
 
 
-def filterPattern(pattern, word, wordlist) -> list[str]:
-    # letters = [HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS, HEBREW_LETTERS]
-    letters = [ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS, ENGLISH_LETTERS]
+def filter_words(pattern: str, guess: str, wordlist: List[str], language: Language) -> list[str]:
+    """
+    filter solution group based on the pattern user got from Wordle.
+    :param wordlist: list of all possible words
+    :param language: language the game is played in, Hebrew / English.
+    :param pattern: the pattern the guess generated, a string of 5 {0,1,2}.
+    :param guess: the guess user made
+    """
+    # letters that have to appear exactly number of times.
+    if language == Language.HEBREW:
+        letters = [HEBREW_LETTERS] * WORD_LENGTH
+    else:
+        letters = [ENGLISH_LETTERS] * WORD_LENGTH
     # letters that have to appear exactly number of times.
     exactly = defaultdict(int)
     # letters that have to appear at least number of times
@@ -97,16 +119,16 @@ def filterPattern(pattern, word, wordlist) -> list[str]:
     greys = []
     for j in range(5):
         if pattern[j] == GREY:
-            letters[j] = letters[j].replace(word[j], "")
-            greys.append(word[j])
+            letters[j] = letters[j].replace(guess[j], "")
+            greys.append(guess[j])
         elif pattern[j] == YELLOW:
-            if word[j] in greys:
+            if guess[j] in greys:
                 return []
-            letters[j] = letters[j].replace(word[j], "")
-            at_least[word[j]] += 1
+            letters[j] = letters[j].replace(guess[j], "")
+            at_least[guess[j]] += 1
         elif pattern[j] == GREEN:
-            letters[j] = word[j]
-            at_least[word[j]] += 1
+            letters[j] = guess[j]
+            at_least[guess[j]] += 1
     # if a letter was greyed & yellowed / green, move it to "exactly".
     for letter in greys:
         if at_least[letter] > 0:
@@ -123,73 +145,42 @@ def filterPattern(pattern, word, wordlist) -> list[str]:
     # filter out based on atLeast and exactly dicts.
     filtered_words = list(filter(lambda word: atLeast_filter(word, atLeast_filters), filtered_words))
     filtered_words = list(filter(lambda word: exactly_filter(word, exactly_filters), filtered_words))
-    # filtered_words = list(filter(lambda word: word_filter(word, at_least, exactly, letters), wordlist))
     return filtered_words
 
 
 class WordleSolver:
-    def __init__(self, hebrew: bool = False):
+    def __init__(self, language: Language = Language.ENGLISH):
         self.patterns = {''.join(i): 0 for i in itertools.product("012", repeat=WORD_LENGTH)}
-        self.hebrew = hebrew
-        if self.hebrew:
-            self.guesses = get_word_list(HEBREW_GUESSES)
-            self.solutions = get_word_list(HEBREW_SOLUTIONS)
-            self.first_words = FIRST_HEBREW_GUESS
+        self.language = language
+        if self.language == Language.HEBREW:
+            self.guesses = load_words_JSON(HEBREW_GUESSES_PATH)
+            self.solutions = load_words_JSON(HEBREW_SOLUTIONS_PATH)
+            self.first_words = load_words_JSON(FIRST_WORDS_HEBREW_PATH)
         else:
-            self.guesses = get_word_list(ENGLISH_GUESSES)
-            self.solutions = get_word_list(ENGLISH_SOLUTIONS)
-            self.first_words = FIRST_ENGLISH_GUESS
+            self.guesses = load_words_JSON(ENGLISH_GUESSES_PATH)
+            self.solutions = load_words_JSON(ENGLISH_SOLUTIONS_PATH)
+            self.first_words = load_words_JSON(FIRST_WORDS_ENGLISH_PATH)
 
-    def filter_solutions(self, pattern: str, guess: str) -> None:
+    def process_heb_word(self, word: str) -> str:
         """
-        filter solution group based on the pattern user got from Wordle.
-        :param pattern: a string of 5 {0,1,2}.
-        :param guess: the guess
+        turn the last letter to finalised ("ot sofit") later or vice versa.
+        :param word: a hebrew word
+        :return: the word with a finalised / de-finalised last letter.
         """
-        # letters that have to appear exactly number of times.
-        if self.hebrew:
-            letters = [HEBREW_LETTERS] * WORD_LENGTH
+        if self.language != Language.HEBREW:
+            return word
+        if word[-1] in REGULAR_TO_FINAL:
+            return word[:-1] + REGULAR_TO_FINAL[word[-1]]
+        elif word[-1] in FINAL_TO_REGULAR:
+            return word[:-1] + FINAL_TO_REGULAR[word[-1]]
         else:
-            letters = [ENGLISH_LETTERS] * WORD_LENGTH
-        exactly = defaultdict(int)
-        # letters that have to appear at least number of times
-        at_least = defaultdict(int)
-        greys = []
-        for j in range(len(pattern)):
-            if pattern[j] == GREY:
-                letters[j] = letters[j].replace(guess[j], "")
-                greys.append(guess[j])
-            elif pattern[j] == YELLOW:
-                if guess[j] in greys:
-                    return
-                letters[j] = letters[j].replace(guess[j], "")
-                at_least[guess[j]] += 1
-            elif pattern[j] == GREEN:
-                letters[j] = guess[j]
-                at_least[guess[j]] += 1
-        # if a letter was greyed & yellowed / green, move it to "exactly".
-        for letter in greys:
-            if at_least[letter] > 0:
-                exactly[letter] = at_least[letter]
-        greys = [x for x in greys if not at_least[x] > 0]
-        # remove all completely greyed chars from possible letters.
-        for j in range(WORD_LENGTH):
-            letters[j] = ''.join('' if c in greys else c for c in letters[j])
-        atLeast_filters = [(k, v) for k, v in at_least.items() if v > 0]
-        exactly_filters = [(k, v) for k, v in exactly.items() if v > 0]
-        # filter out words that don't comply with possible chars for each letter.
-        r = re.compile(
-            REGEX.format(a=letters[0], b=letters[1], c=letters[2], d=letters[3], e=letters[4]))
-        self.solutions = list(filter(r.match, self.solutions))
-        # filter out based on atLeast and exactly letter count
-        self.solutions = list(filter(lambda solution: atLeast_filter(solution, atLeast_filters), self.solutions))
-        self.solutions = list(filter(lambda solution: exactly_filter(solution, exactly_filters), self.solutions))
+            return word
 
-    def calculate_entropy(self, guess: str) -> float:
+    def fast_entropy(self, guess: str) -> float:
         """
         calculate entropy of a given guess - how much information will be attained from guessing the guess in average.
         a score of 1 means halving the solution group once.
-        :param guess:
+        :param guess: the guess we are calculating entropy for
         :return: the entropy of the guess.
         """
         self.patterns = dict.fromkeys(self.patterns, 0)
@@ -197,40 +188,51 @@ class WordleSolver:
         for solution in self.solutions:
             pattern = get_pattern(guess, solution)
             self.patterns[pattern] += 1
-        solutions_count = len(self.solutions)
         entropy = 0
         for pattern in self.patterns:
-            probability = self.patterns[pattern] / solutions_count
+            probability = self.patterns[pattern] / len(self.solutions)
             if probability > 0:
                 entropy += probability * math.log2(1 / probability)
         return entropy
+
+    def calculate_first_word_fast(self) -> None:
+        """
+        Calculate entropies for the first word used, and store results in a JSON file.
+        """
+        res = []
+        for guess in self.guesses:
+            res.append((guess, round(self.fast_entropy(guess), 4)))
+        res.sort(key=lambda x: x[1], reverse=True)
+        filepath = [FIRST_WORDS_ENGLISH_PATH, FIRST_WORDS_HEBREW_PATH][self.language == Language.HEBREW]
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump({"words": res}, f, ensure_ascii=False)
 
     def virtual_solve(self, solution: str) -> int:
         """
         tests the program on a given solution - the program will try to solve the game.
         :param solution: given solution
-        :return: how many guesses it took to solve the given solution, -1 if failed.
-                 -1 if program failed to find given solution in under 6 guesses.
+        :return: how many guesses it took to solve the given solution.
+                 -1 if program failed to find given solution in 6 guesses or under.
         """
         for i in range(NUM_OF_TRIES):
             if len(self.solutions) == 1:
-                return i + 1
-            if i == 0:
+                max_word = self.solutions[0]
+            elif i == 0:
                 max_word = self.first_words[0][0]
             else:
                 max_entropy = -1
                 max_word = ""
                 # find the word with the highest entropy and guess it.
                 for word in self.guesses:
-                    entropy = self.calculate_entropy(word)
+                    entropy = self.fast_entropy(word)
                     if entropy > max_entropy:
                         max_word = word
                         max_entropy = entropy
-                print(max_word)
+            print(i + 1, self.process_heb_word(max_word))
             pattern = get_pattern(max_word, solution)
             if pattern == GREEN * WORD_LENGTH:
                 return i + 1
-            self.filter_solutions(pattern, max_word)
+            self.solutions = filter_words(pattern, max_word, self.solutions, self.language)
         return -1
 
     def interactive_solve(self) -> (str, int):
@@ -242,32 +244,33 @@ class WordleSolver:
         for i in range(NUM_OF_TRIES):
             # stop cases: potential solution group size is lower than 2.
             if len(self.solutions) == 1:
-                return self.solutions[0], i + 1
+                return self.process_heb_word(self.solutions[0]), i + 1
             elif len(self.solutions) == 2:
-                print("potential solutions are:\n", self.solutions)
+                print("potential solutions are:\n", [self.process_heb_word(word) for word in self.solutions])
             # the top guesses are pre-calculated for the first word.
             elif i == 0:
-                print("Top guesses are:\n", self.first_words)
-            # else, calculate what are the best cases and present them to the user.
+                print("Top guesses:\n", [(self.process_heb_word(word), score) for word, score in self.first_words[:5]])
+            # else, calculate what are the best cases and print them to the user.
             else:
                 results = []
+                print(self.solutions)
                 for guess in self.guesses:
-                    if self.hebrew:
-                        guess = process_hebrew_word(guess)
-                    results.append((guess, round(self.calculate_entropy(guess), 2)))
+                    results.append((self.process_heb_word(guess), round(self.fast_entropy(guess), 2)))
                 results.sort(key=lambda x: x[1], reverse=True)
                 print("Top guesses are:\n", results[:5])
-            guess = input("Enter your guess:\n")
-            # go from final letter to non-final letter.
-            if self.hebrew:
-                guess = process_hebrew_word(guess)
+
+            # get user's guess & Wordle's generated pattern from user
+            guess = self.process_heb_word(input("Enter your guess:\n"))
             pattern = input("Enter pattern: (0 for grey, 1 for yellow, 2 for green. i.e \'00112\')\n")
             if pattern == GREEN * WORD_LENGTH:
-                return guess, i + 1
-            self.filter_solutions(pattern, guess)
-        return None, NUM_OF_TRIES
+                return self.process_heb_word(guess), i + 1
+            self.solutions = filter_words(pattern, guess, self.solutions, self.language)
+        return FAIL_MSG, NUM_OF_TRIES
 
 
 if __name__ == '__main__':
-    solver = WordleSolver(True)  # change to False for English version
+    solver = WordleSolver(Language.ENGLISH)  # choose between Language.HEBREW to Language.ENGLISH
     print(solver.interactive_solve())
+    # solver.virtual_solve("חורבן")
+    # solver.calculate_first_word_fast()
+    # solver.calculate_first_word_fast()
